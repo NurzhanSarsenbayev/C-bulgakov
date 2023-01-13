f8558a701d4290f43d6c1facd7929b3e67dbf869
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-01-13 14:29:26 +0600

    task00 finished

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type your number ");
+double numberA = Convert.ToDouble (Console.ReadLine());
+double resultSquare = numberA * numberA;
+Console.Write($"Square of {numberA} is ");
+Console.Write(resultSquare);
