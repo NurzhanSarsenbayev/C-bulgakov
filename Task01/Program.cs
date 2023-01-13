e33b757b64f58ec77861c27c3b0992e09f069ca3
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-01-13 14:48:03 +0600

    Task01 finished

---
+++ Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Type your first number ");
+double numberA = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Type your second number ");
+double numberB = Convert.ToDouble (Console.ReadLine());
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine($"{numberB} is a square root of {numberA}");
+}
+else {
+Console.WriteLine($"{numberB} is not a square root of {numberA} ");
+};
