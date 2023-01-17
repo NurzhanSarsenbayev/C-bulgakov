19ec9de6131335313c1a2093a1f0a42b588b0d16
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-01-17 15:39:58 +0600

    task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -5,3 +5,16 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+bool Square(int arg1, int arg2)
+{
+    return arg1 * arg1 == arg2 || arg2 * arg2 == arg1;
+}
+
+Console.WriteLine("Type two numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool square = Square(num1, num2);
+if (square)  Console.WriteLine("Yes");
+else Console.WriteLine("No");
