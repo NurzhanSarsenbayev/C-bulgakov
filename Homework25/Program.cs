ff37af564548168d7e582f33f37b08b51656545c
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-01-24 22:08:49 +0600

    homework25

---
+++ Homework25/Program.cs
@@ -0,0 +1,20 @@
+﻿// 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//  3, 5 -> 243 (3⁵)
+//  2, 4 -> 16
+
+void Exponent(double number, int expNumb)  // в задании написано цикл, а не программа. Так что написал только метод.
+{
+    double finalnumb = 1;
+    for (int i = 0; i < expNumb; i++)
+    {
+        finalnumb = finalnumb*number;
+        
+    }
+    Console.WriteLine($"{number}, {expNumb} -> {finalnumb}");
+}
+
+Exponent(3,5);
+
+Exponent(2,4);
+
+Exponent(2.5,4);   // так как в условии написано только про натуральную степеньь, сделал вариант с дробным первым числом
