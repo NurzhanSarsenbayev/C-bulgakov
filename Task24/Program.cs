b8432a73be8896bc4ace219d3b76b0f7504cd335
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-01-24 13:48:48 +0600

    task24

---
+++ Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int AddNumber(int num)
+{
+    int sum = 0;
+  for(int i = 0; i<=num; i++)
+  {
+     
+     sum+=i;
+  }
+  return sum;
+}
+
+Console.WriteLine("Type your positive natural number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>0)
+{
+int result = AddNumber(number);
+Console.WriteLine($"{number} -> {result}");
+}
+else Console.WriteLine("Invalid number");
