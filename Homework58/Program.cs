e41980e581f4c2ea556857a9dffc83f64b52c233
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-02-09 01:29:50 +0600

    homework58, homework 60

---
+++ Homework58/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+int[,] GenerateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max - 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} |");
+        }
+        Console.WriteLine();
+    }
+}
+bool isValidForMultiplication(int[,]matrixA , int[,] matrixB)
+{
+    return matrixA.GetLength(1) != matrixB.GetLength(0);
+}
+int[,] MatrixMultiplier(int[,]matrixA , int[,] matrixB)
+{
+    {       
+        int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+        for (int i = 0; i < matrixA.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixB.GetLength(1); j++)
+            {
+                matrixC[i, j] = 0;
+
+                for (int k = 0; k < matrixA.GetLength(1); k++)
+                {
+                    matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+        }
+        return matrixC;
+    }
+}
+
+int[,] matr = GenerateMatrixRndInt(2,2,1,5);
+int[,] matr2 = GenerateMatrixRndInt(2,2,1,5);
+PrintMatrix(matr);
+Console.WriteLine();
+PrintMatrix(matr2);
+bool invalid=isValidForMultiplication(matr, matr2);
+Console.WriteLine();
+if (invalid == true)
+{
+Console.WriteLine("Invalid matrixes. Amount of columns of first matrix isn't equal to amount of rows of second matrix");
+}
+else
+{
+int[,] result = MatrixMultiplier(matr,matr2);
+Console.WriteLine();
+PrintMatrix(result);
+}
---
+++ Homework60/Program.cs
@@ -0,0 +1,41 @@
+﻿// *Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+int[,,] Generate3DMatrixRndInt(int rows, int columns, int depth, int value)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = value;  // это конечно не случайный генератор, но по условию надо 
+                value++;                  // было просто неповторяющиеся двузначные числа.
+            }
+        }
+    }
+    return matrix;
+}
+void Print3DMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k <  matrix.GetLength(2); k++)
+            {
+                Console.Write($"   {matrix[i, j, k]}");
+                Console.Write($"{(i, j, k)}");
+            }
+           
+        }
+        Console.WriteLine();
+    }
+}
+int[,,] matr = Generate3DMatrixRndInt(2, 2, 2, 10);
+Print3DMatrix(matr);
