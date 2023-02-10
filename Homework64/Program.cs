a2f74e6a25836227b377eefd36468dcc02fe63ef
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-02-10 23:08:38 +0600

    update

--- Homework64/Program.cs
+++ Homework64/Program.cs
@@ -6,74 +6,12 @@
 
 void AllNumbersFromNumToOne (int num)
 {
-
 if (num==0) return;
 Console.Write($"{num} ");
    AllNumbersFromNumToOne(num-1);
 }
-
-
 Console.WriteLine("Type in number N");
 int number= Convert.ToInt32(Console.ReadLine());
 Console.Write($"N = {number} -> \" ");
 AllNumbersFromNumToOne(number);
 Console.Write('"');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // void Swap(int num1, int num2)
-// // {
-// //     int tmp;
-// //     tmp = num1;
-// //     num1 = num2;
-// //     num2= tmp;
-// //     Console.WriteLine($"{num1} , {num2}");   
-// // }
-
-// // Console.WriteLine("Type in your number");
-// // int number1 = Convert.ToInt32(Console.ReadLine());
-// // Console.WriteLine("Type in your number");
-// // int number2 = Convert.ToInt32(Console.ReadLine());
-// // Swap(number1, number2);
-// // Console.WriteLine($"{number1} , {number2}");
-
-// // Swap(number1, number2);
-
-// int SwapTwoNumbers (int num1, int num2)
-// {
-//     int tmp = num1;
-//     num1 = num2;
-//     num2 = tmp;
-//     return num1;
-//     return num2;
-// }
-
-// int SumElements (int numM, int numN)
-// {
- 
-//     return numM == numN+1? 0 : numM+SumElements(numM+1, numN);
-// }
-
-// Console.WriteLine("Введите первое число");
-// int numberM = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int numberN = Convert.ToInt32(Console.ReadLine());
-// int sumElements=default;
-//    if(numberM>numberN)
-//     {
-//         SwapTwoNumbers(numberM,numberN);
-//        sumElements = SumElements(numberM,numberN);
-//     }
-// sumElements = SumElements(numberM,numberN);
-// Console.WriteLine(sumElements);
